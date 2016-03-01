f6366aed7c945c945e0a3f20a37af90684959444
Student <student@RU-003.local>
2016-03-01 15:59:39 -0700

    a

--- server/api_routes.js
+++ server/api_routes.js
@@ -9,11 +9,26 @@ var ctrls = require('./controllers.js'),
 // API routes
 // Restaurants
 apiRouter.route('api/restaurants')
-	.post(ctrls.restaurant.create)
+	.post(ctrls.restaurant.upsert)
 	.get(ctrls.restaurant.all)
 
 apiRouter.route('api/restaurants/:_id')
 	.get(ctrls.restaurant.get)
-	.post(ctrls.restaurant.update)
+	.post(ctrls.restaurant.upsert)
 	.delete(ctrls.restaurant.delete)
 
+// Users
+apiRouter.route('api/users')
+	.get(ctrls.users.all)
+	.post(ctrls.users.upsert)
+
+apiRouter.route('api/user/:_id')
+	.get(ctrls.user.get)
+
+// Reviews
+apiRouter.route('api/review/_id')
+	.get(ctrls.review.get)
+	.delete(ctrls.review.delete)
+
+apiRouter.route('api/reviews/')
+	.post(ctrls.review.create)
